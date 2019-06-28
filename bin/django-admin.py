cce1b83e6ff5f6424d7569dd0771f18d4a68388f
Wonho Kim <rhwhdeodhkd@gmail.com>
2019-06-28 16:50:50 +0900

    init commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/likelionWonho/Source/toy-projects/d.agora/d.agora-api/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
